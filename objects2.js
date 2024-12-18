442bb8f14bcb8f66d0032cb5c2443bf22cd26926
Noumankhalil <nomiikhan103@gmail.com>
2024-12-18 23:36:47 +0500

    practice on object-destructuring

--- objects2.js
+++ objects2.js
@@ -19,7 +19,7 @@ let obj1 = { 1: "a", 2: "b" };
 let obj2 = { 3: "c", 4: "d" };
 
 // let obj3 = Object.assign({}, obj1, obj2);
-const obj3 = { ...obj1, ...obj2 };
+// const obj3 = { ...obj1, ...obj2 };
 // console.log(obj3);
 
 const users = [
@@ -41,12 +41,72 @@ users[1].email;
 // console.log(Object.entries(tinderuser));
 // console.log(Object.hasOwnProperty("name"));
 
-const course = {
-  coursename: "js in hindi",
-  price: "999",
+// const course = {
+//   coursename: "js in hindi",
+//   price: "999",
+//   courseinstructor: "hitash",
+// };
+
+// const { courseinstructor: instructor } = course;
+
+// console.log(instructor);
+
+const sym = Symbol("demo1");
+
+const info = {
+  name: "nouman",
+  email: "nomi@gmail.com",
+  age: 22,
+  [sym]: "demo",
+  names: ["ali,asad", "obaid"],
+};
+
+info.name = "ali";
+
+Object.freeze(info);
+info.name = "aaa";
+
+console.log(info);
+
+info.Hello = function () {
+  console.log(`heloo ${name}`);
+};
+
+// console.log(info.name);
+// console.log(info["email"]);
+// console.log(info[sym]);
+
+const tender1 = new Object();
+
+tender1.id = "123";
+tender1.name = "ten";
+tender1.email = "t@gmail.com";
+
+let obj11 = { 1: "a", 2: "b" };
+let ob12 = { 3: "c", 4: "d" };
+
+// let obj13 = { ...obj11, ...ob12 };
+// console.log(obj13);
+
+let demo = [
+  {
+    name: "asd",
+    email: "asd@gmail.com",
+  },
+  {
+    name: "sss",
+    email: "sss@gmail.com",
+  },
+];
+console.log(demo[1]);
+
+let course1 = {
+  cousename: "js in hindi",
+  price: 5555,
   courseinstructor: "hitash",
 };
 
-const { courseinstructor: instructor } = course;
+const { cousename: cn, courseinstructor: cinstructor } = course1;
 
-console.log(instructor);
+console.log(cn);
+console.log(cinstructor);
