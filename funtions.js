cbc705dfe82c9223f819354a0ebf1a56e8435f3c
Noumankhalil <nomiikhan103@gmail.com>
2025-01-01 15:52:47 +0500

    practice on functions

---
+++ funtions.js
@@ -0,0 +1,37 @@
+// function Hello() {
+//   console.log("welcome");
+// }
+
+// Hello();
+
+// function addnums(num1, num2) {
+//   result = num1 + num2;
+//   return result;
+//   console.log("hellooo");
+// }
+
+// a = addnums(5, 4);
+
+// console.log(`result : ${a}`);
+
+function addcart(num) {
+  return num;
+}
+
+addcart(2);
+
+const user = {
+  name: "ali",
+  age: 22,
+};
+
+// function demo(anyobject) {
+//   console.log(`my ame is ${anyobject.name} ad my age is ${anyobject.age}`);
+// }
+
+// demo();
+
+test({
+  product: "asd",
+  price: 555,
+});
