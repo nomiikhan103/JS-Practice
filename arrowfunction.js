ae31ae23d26f80972bb036c28c466c114473fa49
Noumankhalil <nomiikhan103@gmail.com>
2025-01-18 20:55:22 +0500

    this keyword practice

---
+++ arrowfunction.js
@@ -0,0 +1,45 @@
+// const user = {
+//   username: "nouman",
+//   price: 999,
+//   hello: function () {
+//     console.log(`${this.username} , welcome`);
+
+//     console.log(this);
+//   },
+// };
+
+// user.hello();
+// user.username = "kha";
+
+// user.hello(this);
+
+// function chi() {
+//   user = "nouman";
+//   console.log(user.this);
+// }
+
+// const chi = function chi() {
+//   user = "nouman";
+//   console.log(user.this);
+// };
+
+// const chi = () => {
+//   user = "nouman";
+//   console.log(user.this);
+// };
+
+// const chi = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const chi = (num1, num2) => num1 + num2;
+
+// const chi = (num1, num2) => (num1 + num2);
+
+// const chi = (num1, num2) => {
+//   (username = "nouman khalil");
+// };
+
+
+
+console.log(chi(2, 4));
