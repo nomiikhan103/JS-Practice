4477da5adc7513c94692de223ea5340c0ebad02c
Noumankhalil <nomiikhan103@gmail.com>
2025-02-03 20:29:07 +0500

    for_of & for_in loop practice

---
+++ loops_part2.js
@@ -0,0 +1,75 @@
+const names = ["ali", "asad", "obaid"];
+
+for (const name of names) {
+  // console.log(name);
+}
+const map = new Map();
+map.set("pak", "pakistan");
+map.set("ind", "india");
+map.set("fr", "france");
+// console.log(map);
+for (const [key, values] of map) {
+  // console.log(key, ":-", values);
+}
+
+// const info = {
+//   name: "asad",
+//   email: "kha@gmail.com",
+//   city: "peshawar",
+// };
+
+// for (const [key, value] of info) {
+//   console.log(key, ":-", value);
+// }
+
+// forin loop
+
+const info = {
+  name: "asad",
+  email: "kha@gmail.com",
+  city: "peshawar",
+};
+
+for (const key in info) {
+  // console.log(`this is keys of object ${key} and values are : ${info[key]}`);
+}
+
+const programming = ["js", "java", "python", "php"];
+
+for (const program in programming) {
+  // console.log(programming);
+}
+
+const myfriends = ["ali", "asad", "obaid", "usman"];
+
+// myfriends.forEach(function (val) {
+//   console.log(val);
+// });
+
+// myfriends.forEach((item) => {
+//   console.log(item);
+// });
+
+// myfriends.forEach((item, index, arr) => {
+//   console.log(item, index, arr);
+// });
+
+const coding = [
+  {
+    languagename: "javasript",
+    languagefile: "js",
+  },
+  {
+    languagename: "java",
+    languagefile: "java",
+  },
+
+  {
+    languagename: "python",
+    languagefile: "py",
+  },
+];
+
+coding.forEach((item) => {
+  console.log(item.languagename);
+});
